f67a46aa2b7bbf1a11c08f55357813dfa8c2448e
Andrew Welton <awelton@mail.uoguelph.ca>
2015-10-20 15:35:27 -0400

    Should actually generate properly now

--- Initial_Layout.java
+++ Initial_Layout.java
@@ -14,13 +14,15 @@ public class Initial_Layout {
     public Tile[][] PopulateTiles() {
         // x, y
         Tile[][] tiles = new Tile[8][8];
+        //i = x
+        //j = y
         for(int i = 0; i < 8; i++) {
             for(int j = 0; j < 8; j++) {
-                if(j == 1 || j == 6) {
+                if((i == 0 || i == 7) && (j >= 1 && j <= 6)) {
                     Piece newPiece = new Piece(0, "O");
                     Tile tile = new Tile(newPiece, true, false);
                     tiles[i][j] = tile;
-                }else if(i == 1 || i == 6) {
+                }else if((j == 0 || j == 7) && (i >= 1 && i >= 6)) {
                     Piece newPiece = new Piece(1, "X");
                     Tile tile = new Tile(newPiece, true, false);
                     tiles[i][j] = tile;
