2af6728218db919c82657d2311b232bddbaf3aee
Andrew Welton <awelton@mail.uoguelph.ca>
2015-10-19 20:50:58 -0400

    I'm bad
    I used the wrong numbers

--- Initial_Layout.java
+++ Initial_Layout.java
@@ -16,12 +16,12 @@ public class Initial_Layout {
         Tile[][] tiles = new Tile[8][8];
         for(int i = 0; i < 8; i++) {
             for(int j = 0; j < 8; j++) {
-                if(j == 0 || j == 7) {
+                if(j == 1 || j == 6) {
                     Piece newPiece = new Piece(0, "O");
                     Tile tile = new Tile(newPiece, true, false);
                     tiles[i][j] = tile;
                 }
-                if(i == 0 || i == 7) {
+                if(i == 1 || i == 6) {
                     Piece newPiece = new Piece(1, "X");
                     Tile tile = new Tile(newPiece, true, false);
                     tiles[i][j] = tile;
