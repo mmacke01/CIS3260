c763e9525834599c3353c47df6511e1fb4034528
Mark <mwilso14@mail.uoguelph.ca>
2015-10-20 18:20:24 -0400

    pros get pie, and other changes to text

--- Display.java
+++ Display.java
@@ -14,7 +14,7 @@ public class Display {
         
         Tile[][] myTiles; 
         myTiles = board.GetTiles();
-        Peice currentPeice;
+        Piece currentPiece;
         Tile currentTile;
         int owner = 0;
         int name = 0;
@@ -22,10 +22,10 @@ public class Display {
             for (int y = 0; y < 8; y++) {
                 
                 currentTile = myTiles[x][y];
-                currentPeice = currentTile.lookAtPiece();
+                currentPiece = currentTile.lookAtPiece();
                    
                 if (currentTile.lookAtPiece() != null){
-                    //found a peice
+                    //found a Piece
                     owner = currentTile.lookAtPiece().GetOwner();
                             if(owner == Player){
                                 System.out.print("("+currentTile.lookAtPiece().GetId()+")");
@@ -69,10 +69,10 @@ O - - - - - - O
         
     }
 
-    public void DisplayMoveOptions(Board board , Peice SelectedPiece ){
+    public void DisplayMoveOptions(Board board , Piece SelectedPiece ){
     
         int destNum = 1;
-        Peice currentPeice;
+        Piece currentPiece;
         Tile[][] myTiles; 
         myTiles = board.GetTiles();
         
@@ -81,20 +81,20 @@ O - - - - - - O
             for (int y = 0; y < 8; y++) {
                 
                 currentTile = myTiles[x][y];
-                currentPeice = currentTile.lookAtPiece();
+                currentPiece = currentTile.lookAtPiece();
     
                 if(currentTile.GetIsDestination == true){ // if its a destination
                     System.out.print("["+destNum+"]");
                     destNum++;
                     
                 }else{ //not a destination
-                    if(currentPeice!= null){// has a peice
-                        if(currentPeice.GetID() == SelectedPiece.GetID()&& currentPeice.GetOwner() == SelectedPiece.GetOwner()){
+                    if(currentPiece!= null){// has a piece
+                        if(currentPiece.GetID() == SelectedPiece.GetID()&& currentPiece.GetOwner() == SelectedPiece.GetOwner()){
                             System.out.print("("+SelectedPiece.GetOwner()+")");
                         }else{
-                             System.out.print("("+currentPeice.GetOwner()+")");
+                             System.out.print("("+currentPiece.GetOwner()+")");
                         }
-                    }else{// no peice 
+                    }else{// no piece 
                         System.out.print(" - ");
                     }
                 }
@@ -123,26 +123,26 @@ as usual, except wrapped in parentheses. Empty tiles are displayed as ‘-’.*/
 "------------------------------------------------------------------------------\n"+
 "The board is a standard chess/checkers board. Each player begins with 12 \n"+
 "pieces, with6 on either side of the board. For example, Player 1 would have \n"+
-"6 pieces on the northside of the board, and 6 on the south side of the board.\n"+
-"Player 2 would have 6 on thewest and 6 on the east side. The players take\n "+
+"6 pieces on the north side of the board, and 6 on the south side of the board.\n"+
+"Player 2 would have 6 on the west and 6 on the east side. The players take\n "+
 "turns making a move.\n"+
  "-----------------------------------------------------------------------------\n"+
 "\nObjective\n"+
 "------------------------------------------------------------------------------\n"+
 "To win, a player must connect all of his or her pieces in one group. The first\n"+
-"player to doso wins. A piece is considered connected if it is directly or \n"+
-"diagonally adjacent toanother piece. Additionally, a player automatically \n"+
+"player to do so wins. A piece is considered connected if it is directly or \n"+
+"diagonally adjacent to another piece. Additionally, a player automatically \n"+
 "definition.\n"+
 "------------------------------------------------------------------------------\n"+
 "Movement\n"+
 
 "Pieces can move in any direction. A player may move a piece to a tile that \n"+
 "holds anopponent’s piece, and doing so ‘destroys’ the opponents piece. A player’s\n"+
-"piece canjump over their own piece, but they cannot jump over an opponent’s \n"+
-"piece. A player canmove their piece to a tile in the distance according to the\n"+
+"piece can jump over their own piece, but they cannot jump over an opponent’s \n"+
+"piece. A player can move their piece to a tile in the distance according to the\n"+
 "amount of pieces residing on that movement’s direction across the board \n"+
-"(including the selected piece). A piece canbe moved diagonally, and the same \n"+
-"rule applies. For example, if a piece is being movedhorizontally, and there\n"+
+"(including the selected piece). A piece can be moved diagonally, and the same \n"+
+"rule applies. For example, if a piece is being moved horizontally, and there\n"+
 "is one additional piece in the line that the piece is moving upon,\n"+
 "that piece can move two tiles.\n");
     }
@@ -156,7 +156,7 @@ as usual, except wrapped in parentheses. Empty tiles are displayed as ‘-’.*/
         System.out.println("would you like to play again? Y (yes) N (no)");
     }
     public void PromptPiece(){
-        System.out.print("Select a peice: ");
+        System.out.print("Select a piece: ");
     }
     public void PromptDestination(){
         System.out.print("Select a destination: ");
